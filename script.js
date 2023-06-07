dc19faf55d8c32b1886c094a59780efcf815ed79
maximum0034 <135773475+maximum0034@users.noreply.github.com>
2023-06-07 10:40:17 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+    var loadingContainer = document.getElementById("loading-container");
+    loadingContainer.style.display = "none";
+  });
+  setTimeout(function() {
+
+  }, 1000);
